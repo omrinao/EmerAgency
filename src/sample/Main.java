aef56db4656733e8c6cc028b9e83acc0b5a3f168
omrinao <omrinao@post.bgu.ac.il>
2019-06-06 21:44:37 +0300

    fxml done

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("viewEvent.fxml"));
         primaryStage.setTitle("Emer-Agency");
-        Scene scene = new Scene(root, 640, 400);
-        scene.getStylesheets().add(getClass().getResource("/ViewStyle.css").toExternalForm());
+        Scene scene = new Scene(root, 900, 600);
+        scene.getStylesheets().add(getClass().getResource("ViewStyle.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
 
