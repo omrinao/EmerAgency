a287baeac1c80188e300561d0b104a138031cf0c
omrinao <omrinao@post.bgu.ac.il>
2019-06-07 18:44:24 +0300

    view workout

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,75 @@
 package Controller;
+import Models.Model;
+import View.*;
+
+import java.util.ArrayList;
 
 public class Controller {
+
+    //login screen
+
+    public String login_send(String username, String password){
+        return "success";
+    }
+
+
+    //create event screen
+
+    public String create_event(String title, ArrayList<String> categories, String description, ArrayList<String> org){
+        return "success";
+    }
+
+    public ArrayList<String> getCategories(){
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("Fire");
+        toReturn.add("Murder");
+        toReturn.add("Fired");
+        toReturn.add("Murdered");
+        toReturn.add("Firedede");
+        return toReturn;
+    }
+
+    public ArrayList<String> getOrg(){
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("Police");
+        toReturn.add("Fire Fighters");
+        return toReturn;
+    }
+
+
+    //post update screen
+
+    public String post_update(String category, String event, String description){
+        return "success";
+    }
+
+    public ArrayList<String> getEvents(String category, String username){
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("Fire very hard fire");
+        toReturn.add("Murder");
+        toReturn.add("Fired");
+        toReturn.add("Murdered");
+        toReturn.add("Firedede");
+        return toReturn;
+    }
+
+
+    //view event screen
+
+    public ArrayList<String> getCategoriesToView(String username){
+        return new ArrayList<String>();
+    }
+
+
+    public ArrayList<String> getEventToView(String category){
+        return new ArrayList<>();
+    }
+
+
+    //view event details screen
+
+    public ArrayList<String> getEventDetails(String category, String event){
+        return new ArrayList<>();
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import Controller.Controller;
+import Models.Model;
+import View.AView;
 
 public class Main extends Application {
+    public static Stage pStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println(getClass().getResource("fxml/mainView.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/mainView.fxml"));
-        primaryStage.setTitle("Emer-Agency");
+
+
+        pStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+
+
+        pStage.setTitle("Emer-Agency");
         Scene scene = new Scene(root, 900, 600);
         scene.getStylesheets().add(getClass().getResource("css/ViewStyle.css").toExternalForm());
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        pStage.setScene(scene);
+        pStage.show();
 
     }
 
