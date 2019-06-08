9fda442d3f19734b969f060ca9eb844cfc092722
omrinao <38496755+omrinao@users.noreply.github.com>
2019-06-08 17:39:25 +0300

    View and fxml

--- src/View/postUpdateView.java
+++ src/View/postUpdateView.java
@@ -16,11 +16,16 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class postUpdateView extends AView {
-    public ChoiceBox cb_category;
+
     public ChoiceBox cb_event;
     public TextArea ta_description;
     public Button btn_postUpdate;
     public Button btn_back;
+    public CheckBox cb_c1;
+    public CheckBox cb_c2;
+    public CheckBox cb_c3;
+    public CheckBox cb_c4;
+    public CheckBox cb_c5;
 
     /**
      * method to set main screen
@@ -48,22 +53,63 @@ public class postUpdateView extends AView {
 
 
     public void prepareView(MouseEvent mouseEvent){
-        if (cb_category.getItems().size() == 0) {
+        if (cb_c1.getText().isEmpty()) {
             ArrayList<String> cat = _controller.getCategories();
-            cb_category.getItems().addAll(cat);
+            cb_c1.setText(cat.get(0));
+            cb_c2.setText(cat.get(1));
+            cb_c3.setText(cat.get(2));
+            cb_c4.setText(cat.get(3));
+            cb_c5.setText(cat.get(4));
         }
     }
 
 
     public void set_events(MouseEvent mouseEvent){
-        if (!cb_category.getSelectionModel().getSelectedItem().toString().isEmpty()){
-            if (cb_event.getItems().size() == 0) {
-                String cat = "";
-                cat = cb_category.getValue().toString();
-                ArrayList<String> events = _controller.getEvents(cat, loginView.username);
-                cb_event.getItems().addAll(events);
-                cb_event.setDisable(false);
-            }
+        ArrayList<String> events = new ArrayList<>();
+        if (cb_c1.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c1.getText(), loginView.username));
+            events.addAll(_controller.getEvents(cb_c1.getText(), loginView.username));
+        }
+        if (cb_c2.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c2.getText(), loginView.username));
+            events.addAll(_controller.getEvents(cb_c2.getText(), loginView.username));
+        }
+        if (cb_c3.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c3.getText(), loginView.username));
+            events.addAll(_controller.getEvents(cb_c3.getText(), loginView.username));
+        }
+        if (cb_c4.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c4.getText(), loginView.username));
+            events.addAll(_controller.getEvents(cb_c4.getText(), loginView.username));
+        }
+        if (cb_c5.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c5.getText(), loginView.username));
+            events.addAll(_controller.getEvents(cb_c5.getText(), loginView.username));
+        }
+        if (!cb_c1.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c1.getText(), loginView.username));
+        }
+        if (!cb_c2.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c2.getText(), loginView.username));
+        }
+        if (!cb_c3.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c3.getText(), loginView.username));
+        }
+        if (!cb_c4.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c4.getText(), loginView.username));
+        }
+        if (!cb_c5.isSelected()){
+            events.removeAll(_controller.getEvents(cb_c5.getText(), loginView.username));
+        }
+
+        cb_event.getItems().clear();
+
+        if (events.size() > 0){
+            cb_event.getItems().addAll(events);
+            cb_event.setDisable(false);
+        }
+        else{
+            cb_event.setDisable(true);
         }
     }
 
@@ -72,15 +118,30 @@ public class postUpdateView extends AView {
      * @param mouseEvent - mouse click on 'back' button on create event screen
      */
     public void send_update(MouseEvent mouseEvent) {
-        String category = cb_category.getValue().toString();
+        ArrayList<String> categories = new ArrayList<>();
+        if (cb_c1.isSelected()){
+            categories.add(cb_c1.getText());
+        }
+        if (cb_c2.isSelected()){
+            categories.add(cb_c2.getText());
+        }
+        if (cb_c3.isSelected()){
+            categories.add(cb_c3.getText());
+        }
+        if (cb_c4.isSelected()){
+            categories.add(cb_c4.getText());
+        }
+        if (cb_c5.isSelected()){
+            categories.add(cb_c5.getText());
+        }
         String title = cb_event.getValue().toString();
         String description = ta_description.getText();
 
-        if (title.isEmpty() || description.isEmpty() || category.isEmpty()){
+        if (title.isEmpty() || description.isEmpty()){
             popProblem("You need to fill in all fields\n");
             return;
         }
-        String response = _controller.post_update(category, title, description);
+        String response = _controller.post_update(categories, title, description);
         if (!response.equals("success")) {
             popProblem("Create event failed!\n" +
                     "Make sure you typed in proper details");
